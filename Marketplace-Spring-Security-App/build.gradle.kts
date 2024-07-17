67ff62946ee2d8095d82871fe91f1e5fb2164242
Yevhen-Tkachenko <yevhen.tkachenko.1@gmail.com>
2024-07-17 13:20:06 +0300

    Marketplace app: - add dependencies

--- Marketplace-Spring-Security-App/build.gradle.kts
+++ Marketplace-Spring-Security-App/build.gradle.kts
@@ -1,15 +1,28 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.1.0"
+    id("io.spring.dependency-management") version "1.1.0"
 }
 
 group = "yevhent.demo.springboot.springsecurity"
-version = "unspecified"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    implementation("com.h2database:h2")
+    compileOnly("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+
+    testImplementation("org.springframework.security:spring-security-test")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
